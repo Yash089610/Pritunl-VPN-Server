c2f1177afcffe964b6f9d90f7e2ef16d6321a17b
Baalaji <baalajimaestro@computer4u.com>
2018-09-28 13:21:04 +0530

    Update vpn-server.sh

--- vpn-server.sh
+++ vpn-server.sh
@@ -24,10 +24,12 @@ echo "[Install]" >> /lib/systemd/system/mongod.service
 echo "WantedBy=multi-user.target" >> /lib/systemd/system/mongod.service
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
 echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 42F3E95A2C4F08279C4960ADD68FA50FEA312927
+sudo tee -a /etc/apt/sources.list.d/pritunl.list << EOF
+deb http://repo.pritunl.com/stable/apt bionic main
+EOF
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
-apt-get update -y
-apt-get install pritunl mongodb-org -y
+apt-get update
+apt-get --assume-yes install pritunl-link
 systemctl start pritunl mongod
 systemctl enable pritunl mongod
 # Collect setup key
