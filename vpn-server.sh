f2b3fd6097d802a0bfb1a8b7fc018ae4d2ac061a
Baalaji <baalajimaestro@computer4u.com>
2018-09-28 14:33:25 +0530

    Create vpn-server.sh

---
+++ vpn-server.sh
@@ -0,0 +1,32 @@
+#! /bin/bash		
+ SERVER_NAME=bot-vpn-server		
+ gcloud compute instances create $SERVER_NAME \		
+ --machine-type "f1-micro" \		
+ --image-family ubuntu-1804-lts \		
+ --image-project "ubuntu-os-cloud" \		
+ --boot-disk-size "10" \		
+ --boot-disk-type "pd-standard" \		
+ --boot-disk-device-name "vpn-server" \		
+ --tags https-server,http-server \		
+ --zone us-central1-f \		
+ --labels ready=true \		
+ --can-ip-forward \		
+ --metadata startup-script='#! /bin/bash		
+ sudo su -		
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.6.list
+echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+apt-get --assume-yes update
+apt-get --assume-yes upgrade
+apt-get --assume-yes install pritunl mongodb-org
+systemctl start pritunl mongod
+systemctl enable pritunl mongod
+ # Collect setup key		
+ echo "setup key follows:"		
+ pritunl setup-key		
+ '		
+ IP=$(gcloud compute instances describe $SERVER_NAME --zone us-central1-f | grep natIP | cut -d: -f2 | sed 's/^[ \t]*//;s/[ \t]*$//')		
+ gcloud compute firewall-rules create vpn-allow-8787 --allow tcp:8787 --network default --priority 65535 --source-ranges $IP/32		
+ gcloud compute firewall-rules create vpn-allow-3838 --allow tcp:3838 --network default --priority 65535 --source-ranges $IP/32		
+ echo "VPN server will be available for setup at https://$IP in a few minutes."
